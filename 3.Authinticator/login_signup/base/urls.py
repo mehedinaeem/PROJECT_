1513459a4931327159eed0c7276fea80440fdb71
mehedinaeem <mehedinaeem00@gmail.com>
2024-11-19 16:01:15 +0600

    update login signup

--- 3.Authinticator/login_signup/base/urls.py
+++ 3.Authinticator/login_signup/base/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
    path('home/',views.home,name='home'),
    path("accounts/", include("django.contrib.auth.urls")),
+   path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
    path("signup/", views.authView,name="authView"),
 ]
--- 3.Authinticator/login_signup/login_signup/views.py
+++ 3.Authinticator/login_signup/login_signup/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def pro_home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')

79ba6a8f5527264a88e11115eee09d2dfd749291
mehedinaeem <mehedinaeem00@gmail.com>
2024-11-20 17:28:45 +0600

    Complete style all component using css

--- 3.Authinticator/login_signup/base/urls.py
+++ 3.Authinticator/login_signup/base/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
    path('home/',views.home,name='home'),
    path("accounts/", include("django.contrib.auth.urls")),
    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-   path("signup/", views.authView,name="authView"),
+   path("signup/", views.authView,name="signup"),
 ]
